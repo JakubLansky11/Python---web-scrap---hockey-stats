53598c0aa137e4c05bcaaa6dad4bf17b1c70b406
Kuba Lansky <159712137+JakubLansky11@users.noreply.github.com>
2024-09-05 15:47:30 +0200

    Python program
    Python program for web scrap - statistics about Czech ice-hockey match.

--- Hockey-stats.py
+++ Hockey-stats.py
@@ -15,37 +15,18 @@ url_hlavni = input("Zadej link na zápas české hokejové extraligy: ")
 tabulka_hokej = get(url_hlavni)
 soup1 = bs(tabulka_hokej.text, "html.parser")
 souhrn= soup1.find_all("table", {"class": "table-last-right"})
-titulek = soup1.find("title").text
-tymy = []
 souhrn_tymy= soup1.find_all("h2", {"class": "short"})
-for tym in souhrn_tymy:
-    tymy.append(tym.text)
-radek_0 = []
-sloupec_cas = []
-sloupec_tym = []
-sloupec_gol = []
-sloupec_asistence = []
-sloupec_typ_golu = []
+tymy = [tym.text for tym in souhrn_tymy]
+radek_0 = [soup1.find("title").text]
 prazdny = []
 radek_1 = ["Čas", "Tým", "Střelec gólu", "Asistence", "Typ gólu (přesilovka,..)", "Hráči u vstřeleného gólu", "Hráči u inkasovaného gólu"]
-
-polozky_2 = []
+polozky = []
 for polozka in souhrn:
     jmeno = polozka.find_all("td")
-    polozky_2.append(jmeno)
-goly_prvni_tretina = polozky_2[0]
-goly_druha_tretina = polozky_2[1]
-goly_treti_tretina = polozky_2[2]
-goly_cas = []
-goly_tym = []
-goly_strelci_upravit = []
-goly_strelci = []
-goly_asistence_upravit = []
-goly_asistence = []
-goly_typ = []
-goly_typ_2 = []
-goly_hraci_plus = []
-goly_hraci_minus = []
+    polozky.append(jmeno)
+goly_prvni_tretina = polozky[0]
+goly_druha_tretina = polozky[1]
+goly_treti_tretina = polozky[2]
 max_index_1 = (len(goly_prvni_tretina))
 nasobek_1 = int(max_index_1/7)
 max_index_2 = (len(goly_druha_tretina))
@@ -53,55 +34,45 @@ nasobek_2 = int(max_index_2/7)
 max_index_3 = (len(goly_treti_tretina))
 nasobek_3 = int(max_index_3/7)
 
-for x in range(0, nasobek_1):
-    goly_cas.append(goly_prvni_tretina[0 + 7*x].text)
+goly_cas = [goly_prvni_tretina[0 + 7*x].text for x in range(0, nasobek_1)]
 for x in range(0, nasobek_2):
     goly_cas.append(goly_druha_tretina[0 + 7*x].text)
 for x in range(0, nasobek_3):
     goly_cas.append(goly_treti_tretina[0 + 7*x].text)
-for x in range(0, nasobek_1):
-    goly_tym.append(goly_prvni_tretina[1 + 7*x].text)
+goly_tym = [goly_prvni_tretina[1 + 7*x].text for x in range(0, nasobek_1)]
 for x in range(0, nasobek_2):
     goly_tym.append(goly_druha_tretina[1 + 7*x].text)
 for x in range(0, nasobek_3):
     goly_tym.append(goly_treti_tretina[1 + 7*x].text)
-for x in range(0, nasobek_1):
-    goly_strelci_upravit.append(goly_prvni_tretina[2 + 7*x].text)
+goly_strelci_upravit = [goly_prvni_tretina[2 + 7*x].text for x in range(0, nasobek_1)]
 for x in range(0, nasobek_2):
     goly_strelci_upravit.append(goly_druha_tretina[2 + 7*x].text)
 for x in range(0, nasobek_3):
     goly_strelci_upravit.append(goly_treti_tretina[2 + 7*x].text)
-for strelec in goly_strelci_upravit:
-    goly_strelci.append(strelec[1:-1])
-for x in range(0, nasobek_1):
-    goly_asistence_upravit.append(goly_prvni_tretina[3 + 7*x].text)
+goly_strelci = [strelec[1:-1] for strelec in goly_strelci_upravit]
+goly_asistence_upravit = [goly_prvni_tretina[3 + 7*x].text for x in range(0, nasobek_1)]
 for x in range(0, nasobek_2):
     goly_asistence_upravit.append(goly_druha_tretina[3 + 7*x].text)
 for x in range(0, nasobek_3):
     goly_asistence_upravit.append(goly_treti_tretina[3 + 7*x].text)
-for strelec in goly_strelci_upravit:
-    goly_strelci.append(strelec[1:-1])
-for asistent in goly_asistence_upravit:
-    goly_asistence.append(asistent.strip("\n"))
-for x in range(0, nasobek_1):
-    goly_typ.append(goly_prvni_tretina[4 + 7*x].text)
+goly_asistence = [asistent.strip("\n") for asistent in goly_asistence_upravit]
+goly_typ = [goly_prvni_tretina[4 + 7*x].text for x in range(0, nasobek_1)]
 for x in range(0, nasobek_2):
     goly_typ.append(goly_druha_tretina[4 + 7*x].text)
 for x in range(0, nasobek_3):
-    goly_typ.append(goly_treti_tretina[4 + 7*x].text)
-for x in range(0, nasobek_1):
-    goly_hraci_plus.append(goly_prvni_tretina[5 + 7*x].text)
+    goly_typ.append(goly_treti_tretina[4 + 7*x].text) 
+goly_hraci_plus = [goly_prvni_tretina[5 + 7*x].text for x in range(0, nasobek_1)]
 for x in range(0, nasobek_2):
     goly_hraci_plus.append(goly_druha_tretina[5 + 7*x].text)
 for x in range(0, nasobek_3):
     goly_hraci_plus.append(goly_treti_tretina[5 + 7*x].text)
-for x in range(0, nasobek_1):
-    goly_hraci_minus.append(goly_prvni_tretina[6 + 7*x].text)
+goly_hraci_minus = [goly_prvni_tretina[6 + 7*x].text for x in range(0, nasobek_1)]
 for x in range(0, nasobek_2):
     goly_hraci_minus.append(goly_druha_tretina[6 + 7*x].text)
 for x in range(0, nasobek_3):
     goly_hraci_minus.append(goly_treti_tretina[6 + 7*x].text)
 
+goly_typ_2 = []
 delka_typ = len(goly_typ)
 for x in range(0, delka_typ):
     if goly_typ[x] == "5/5":
@@ -125,15 +96,14 @@ for x in range(0, delka_typ):
     else:
         goly_typ_2.append("TS/jiné")
 
-pocet_radku = len(goly_cas) + 1
-radek_0.append(titulek)
-
 soubor = open("hokej-statistiky-zapas.csv", mode="w", newline="")
 zapisovac = csv.writer(soubor, delimiter =";")
 zapisovac_2 = csv.writer(soubor)
 zapisovac_2.writerow(radek_0)
+zapisovac.writerow(prazdny)
 zapisovac.writerow(radek_1)
 
+pocet_radku = len(goly_cas) + 1
 for x in range(2, pocet_radku + 1):
     radek_x = []
     radek_x.append(goly_cas[x - 2])
@@ -147,36 +117,24 @@ for x in range(2, pocet_radku + 1):
 
 pocet_golu_domaci = goly_tym.count(tymy[0])
 pocet_golu_hoste = goly_tym.count(tymy[1])
-vysledek_text = (f"Výsledek zápasu {tymy[0]} : {tymy[1]} - {pocet_golu_domaci} : {pocet_golu_hoste} po základní hrací době")
-vysledek = []
-vysledek.append(vysledek_text)
 zapisovac.writerow(prazdny)
-zapisovac.writerow(vysledek)
+zapisovac.writerow([f"Výsledek zápasu {tymy[0]} : {tymy[1]} - {pocet_golu_domaci} : {pocet_golu_hoste} po základní hrací době"])
 zapisovac.writerow(prazdny)
 
 # Pokud se hrálo prodloužení a/nebo nájezdy, tak i z nich doplníme do souboru údaje.
-casti_zapasu = []
 casti = soup1.find_all("h3", {"class": "m-t-0"})
-for cast in casti:
-    casti_zapasu.append(cast.text)
+casti_zapasu = [cast.text for cast in casti]
 if ("Prodloužení" in casti_zapasu and not "Nájezdy" in casti_zapasu):
-    prodlouzeni = []
-    gol_prodlouzeni = polozky_2[3]
+    gol_prodlouzeni = polozky[3]
     radek_prodlouzeni = []
-    prodlouzeni.append("Prodloužení - vítězný gól: ")
-    zapisovac.writerow(prodlouzeni)
-    gol_prodlouzeni_cas = (gol_prodlouzeni[0].text)
-    gol_prodlouzeni_tym = (gol_prodlouzeni[1].text)
+    zapisovac.writerow(["Prodloužení - vítězný gól: "])
     gol_prodlouzeni_strelec_upravit = (gol_prodlouzeni[2].text)
     gol_prodlouzeni_strelec = gol_prodlouzeni_strelec_upravit[1:-1]
-    gol_prodlouzeni_asistence = (gol_prodlouzeni[3].text)
     gol_prodlouzeni_typ = (gol_prodlouzeni[4].text)
-    gol_prodlouzeni_plus = (gol_prodlouzeni[5].text)
-    gol_prodlouzeni_minus = (gol_prodlouzeni[6].text)
-    radek_prodlouzeni.append(gol_prodlouzeni_cas)
-    radek_prodlouzeni.append(gol_prodlouzeni_tym)
+    radek_prodlouzeni.append(gol_prodlouzeni[0].text)
+    radek_prodlouzeni.append(gol_prodlouzeni[1].text)
     radek_prodlouzeni.append(gol_prodlouzeni_strelec)
-    radek_prodlouzeni.append(gol_prodlouzeni_asistence)
+    radek_prodlouzeni.append(gol_prodlouzeni[3].text)
     if gol_prodlouzeni_typ == "5/5":
         radek_prodlouzeni.append("5 na 5")
     elif gol_prodlouzeni_typ == "5/4":
@@ -197,16 +155,13 @@ if ("Prodloužení" in casti_zapasu and not "Nájezdy" in casti_zapasu):
         radek_prodlouzeni.append("Oslabení 3 proti 5")
     else:
         radek_prodlouzeni.append("TS/jiné")
-    radek_prodlouzeni.append(gol_prodlouzeni_plus)
-    radek_prodlouzeni.append(gol_prodlouzeni_minus)
+    radek_prodlouzeni.append(gol_prodlouzeni[5].text)
+    radek_prodlouzeni.append(gol_prodlouzeni[6].text)
     zapisovac.writerow(radek_prodlouzeni)
 if "Nájezdy" in casti_zapasu:
-    najezdy_byly = []
-    najezdy_byly.append("Nájezdy - vítězný nájezd: ")
-    zapisovac.writerow(najezdy_byly)
+    zapisovac.writerow(["Nájezdy - vítězný nájezd: "])
     radek_vitezny_najezd = []
-    najezdy_hraci = []
-    vitezny_najezd = polozky_2[4]
+    vitezny_najezd = polozky[4]
     vitezny_najezd_strelec = vitezny_najezd[2].text
     radek_vitezny_najezd.append(vitezny_najezd[0].text)
     radek_vitezny_najezd.append(vitezny_najezd[1].text)
@@ -216,23 +171,10 @@ if "Nájezdy" in casti_zapasu:
     souhrn_najezdy = soup1.find_all("table", {"class": ""})
     vsechny_najezdy = souhrn_najezdy[-5].find_all("td")
     delka_najezdu = int(len(vsechny_najezdy)/4)
-    najezdy_poradi = []
-    najezdy_tym = []
-    najezdy_vysledek = []
-    najezdy_hrac_upravit = []
-    najezdy_hrac = []
-    for x in range(0, delka_najezdu):
-        najezdy_poradi.append(vsechny_najezdy[0 + 4*x].text)
-    for x in range(0, delka_najezdu):
-        najezdy_tym.append(vsechny_najezdy[1 + 4*x].text)
-    for x in range(0, delka_najezdu):
-        najezdy_vysledek.append(vsechny_najezdy[2 + 4*x].text)
-    for x in range(0, delka_najezdu):
-        najezdy_hrac_upravit.append(vsechny_najezdy[3 + 4*x].text)
-    for hrac in najezdy_hrac_upravit:
-        najezdy_hrac.append(hrac[1:-6])
-    zapisovac.writerow(najezdy_poradi)
-    zapisovac.writerow(najezdy_tym)
-    zapisovac.writerow(najezdy_vysledek)
+    najezdy_hrac_upravit = [vsechny_najezdy[3 + 4*x].text for x in range(0, delka_najezdu)]
+    najezdy_hrac = [hrac[1:-6] for hrac in najezdy_hrac_upravit]
+    zapisovac.writerow([vsechny_najezdy[0 + 4*x].text for x in range(0, delka_najezdu)])
+    zapisovac.writerow([vsechny_najezdy[1 + 4*x].text for x in range(0, delka_najezdu)])
+    zapisovac.writerow([vsechny_najezdy[2 + 4*x].text for x in range(0, delka_najezdu)])
     zapisovac.writerow(najezdy_hrac)
 soubor.close()
